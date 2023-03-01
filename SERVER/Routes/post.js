4a40dcdd120ce0f2dd25892b5462a23c173bd1cf
SARB <mrsinghbusiness05@gmail.com>
2023-03-01 18:16:35 +0530

    Feat - /mypost & /allpost working

--- SERVER/Routes/post.js
+++ SERVER/Routes/post.js
@@ -6,6 +6,18 @@ const router = express.Router();
 const requireLogin = require('../Middleware/RequireLogin');
 const Post = require('../Models/Post');
 
+
+router.get('/allpost' , (req,res) => {
+    Post.find()
+    .populate("postedBy" , "_id name")
+    .then(posts => {
+        res.json({posts})
+    })
+    .catch(err => {
+        console.log(err)
+    })
+})
+
 router.post('/createpost' , requireLogin ,(req,res) => {
     const {title,body} = req.body;
 
@@ -24,6 +36,18 @@ router.post('/createpost' , requireLogin ,(req,res) => {
     .catch(err => {
         console.log(err)
     })
+})
+
+// post for Specific  Logged in  User
+router.get('/mypost' ,requireLogin  , (req,res) => {
+        Post.find({postedBy : req.user._id})
+         .populate("postedBy" ,"_id name")
+         .then(mypost => {
+            res.json({mypost})
+         })
+         .catch(err => {
+            console.log(err)
+         })
 })
 
 
