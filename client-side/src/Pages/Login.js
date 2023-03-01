5e6e12a870c32728e9d9565b79beddd881045be5
SARB <mrsinghbusiness05@gmail.com>
2023-03-02 04:05:17 +0530

    Feat - Login/Signup UI Done

--- client-side/src/Pages/Login.js
+++ client-side/src/Pages/Login.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div> 
-      Login
+            <div className = "mycard-outercontainer" style = {{padding:'5% 12%',width:'50%'}}> 
+                <div className = "mycard-container" style = {{backgroundColor:'whitesmoke',padding:'4%'}}>
+                    <input type = "text"  placeholder = "email" />
+                    <input type = "text"  placeholder = "password" />
+                    <button> Login  </button> 
+                    <span>
+                      <Link to = "/login"> Don't have an Account ? </Link>
+                    </span>
+                </div>
+           </div>
     </div>
   )
 }
--- client-side/src/Pages/Signup.js
+++ client-side/src/Pages/Signup.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
     <div> 
-      Signup
+                         <h3> Signup here </h3>  
+          <div className="signup-outer-container" style = {{padding:'5% 12%',width:'50%'}}>
+                <div className="signup-container" style = {{backgroundColor:'whitesmoke',padding:'4%'}}>
+                    <input type = "text" placeholder='Enter your name' />
+                    <input type = "email" placeholder='Enter your email here' />
+                    <input type = "password" placeholder='Enter your password here' />
+                    <button> Signup here </button>
+                    <span>
+                      <Link to = "/login"> Already have an Account ? </Link>
+                    </span>
+                </div>
+          </div>
     </div>
   )
 }
