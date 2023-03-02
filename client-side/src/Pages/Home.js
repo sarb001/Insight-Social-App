e880bbfb30c49661d2b1768403d10fa7525e9a4f
SARB <mrsinghbusiness05@gmail.com>
2023-03-02 09:24:02 +0530

    Update - Home UI Updated

--- client-side/src/Pages/Home.js
+++ client-side/src/Pages/Home.js
@@ -3,7 +3,42 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h3> All posts are shown here </h3>
+          <div className = "home-outer-container" style = {{margin:'5% 15%'}}> 
+              <h3> All posts are shown here </h3>
+            <div className = "home-container" style = {{backgroundColor:'lightseagreen',width:'40%',padding:'2%'}} >
+                <div className = "card-container">
+                    <div className = "card-content">
+                        <h5> ramesh </h5>
+                        <div className = "card-imge">
+                          <span> <img src = "/Snowintrees.avif" style = {{width:'80%'}} /> </span>  
+                        </div>
+                        <div className="card-content">
+                        <i className='material-icons' style = {{color:'red'}}> favorite </i>
+                          <span> title </span>
+                          <h4> This is amazing post  </h4>
+                          <input type = "text" placeholder='Add a Comment' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className = "home-container" style = {{backgroundColor:'lightseagreen',width:'40%'}} >
+                <div className = "card-container">
+                    <div className = "card-content">
+                        <h5> ramesh </h5>
+                        <div className = "card-imge">
+                          <span> <img src = "/Snowintrees.avif" style = {{width:'80%'}} /> </span>  
+                        </div>
+                        <div className="card-content">
+                        <i className='material-icons' style = {{color:'red'}}> favorite </i>
+                          <span> title </span>
+                          <h4> This is amazing post  </h4>
+                          <input type = "text" placeholder='Add a Comment' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
     </div>
   )
 }
